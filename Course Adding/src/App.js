40910c3e89fe3ab6cf4570b0cafeaea103903c6e
Ahmet Emin Kolbasar <aeminkolbasar@gmail.com>
2023-05-05 17:45:44 +0300

    Basic Course Adding Project
    Basic Course Adding Project with React

---
+++ Course Adding/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import Course from "./Course";
+import React, { useState } from "react";
+
+function getRandomCourse() {
+  const courseArray = ["Angular", "Bootstrap", "Ccsharp", "KompleWeb"];
+  return courseArray[Math.floor(Math.random() * courseArray.length)];
+}
+
+function App() {
+  const [courses, setcourse] = useState([]);
+
+  const handleClick = () => {
+    setcourse([...courses, getRandomCourse()]);
+  };
+  const courseList = courses.map((course, index) => {
+    return <Course key={index} courseName={course} />;
+  });
+
+  return (
+    <div className="App">
+      <button className="appButton" onClick={handleClick}>
+        Kurs Ekle
+      </button>
+      <div className="courseList">{courseList}</div>
+    </div>
+  );
+}
+
+export default App;
